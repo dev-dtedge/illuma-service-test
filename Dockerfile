e8e7f5f9f0f40beec9d22282900041e254a69ff6
Jeremy Whittington <jeremy.whittington@gmail.com>
2017-11-30 09:40:04 -0600

    updates

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,5 @@ RUN apt-get install -y python-pip python-dev build-essential curl
 COPY . /app
 WORKDIR /app
 RUN pip install -r requirements.txt
-EXPOSE 80
 ENTRYPOINT ["python"]
 CMD ["main.py"]
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-
+ 
 RESULT = {
 	"probabilities": [
 		{
@@ -32,4 +32,4 @@ class Predict(Resource):
 api.add_resource(Predict, '/voice/predict', methods=['GET', 'POST'])
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=80)
+    app.run(debug=True,host='0.0.0.0',port=10002)
