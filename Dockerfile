84e8a3cb2004242408cd569f1274a644c7c6d258
Jeremy Whittington <jeremy.whittington@gmail.com>
2017-11-30 09:29:50 -0600

    updates

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM ubuntu:latest
 MAINTAINER Jeremy Whittington "jeremy.whittington@gmail.com"
 RUN apt-get update -y
-RUN apt-get install -y python-pip python-dev build-essential
+RUN apt-get install -y python-pip python-dev build-essential curl
 COPY . /app
 WORKDIR /app
 RUN pip install -r requirements.txt
+EXPOSE 80
 ENTRYPOINT ["python"]
 CMD ["main.py"]
--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from flask import Flask, request
-from flask_restful import reqparse, abort, Api, Resource
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
-parser = reqparse.RequestParser()
-parser.add_argument('task')
 
 RESULT = {
 	"probabilities": [
@@ -29,10 +27,9 @@ class Predict(Resource):
         return RESULT
 
     def post(self):
-        args = parser.parse_args()
         return RESULT
 
 api.add_resource(Predict, '/voice/predict', methods=['GET', 'POST'])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0',port=80)
